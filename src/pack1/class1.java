2e29e18b7b41b4627737372c15555f5a26db46e9
vijaycseityahoo <vijaycseit@yahoo.com>
2025-04-01 17:46:03 +0530

    Update class1.java

--- src/pack1/class1.java
+++ src/pack1/class1.java
@@ -5,6 +5,7 @@ public class class1 {
 	public static void main(String[] args) {
 		System.out.println("print this statement 1");
 		System.out.println("print 1");
+		System.out.println("print 1");
 	}
 	
 }
